677b027bc4b18f67a66010e8c18df253c13a20be
isaink <inks.minds@gmail.com>
2018-12-04 16:59:54 -0500

    first commit

--- main.js
+++ main.js
@@ -2,5 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.post("/")
+app.get("", (req, res) => { // query parameters through GET requests
+
+})
+
+
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log("You are listening to port 3000");
+});
